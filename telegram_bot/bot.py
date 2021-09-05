30b762934f6ddefd3751db3386534c5489b3477b
Jabloco <50886478+Jabloco@users.noreply.github.com>
2021-09-05 20:23:24 +0300

    Поправил инлайн комментарии по PEP 8. Удалил закомментированный код

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -2,22 +2,22 @@ import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
 
-FORMAT = '%(asctime)s - %(message)s' # задаем формат атрибуту format: дата - сообщение
+FORMAT = '%(asctime)s - %(message)s'  # задаем формат атрибуту format: дата - сообщение
 logging.basicConfig(filename="bot.log", level=logging.INFO, format=FORMAT)
 
 def talk_to_me(update, context):
     text = update.message.text
-    print(text) # вывод в консоль
-    update.message.reply_text(text) # вывод в телеграм
+    print(text)  # вывод в консоль
+    update.message.reply_text(text)  # вывод в телеграм
 
 def greet(update,context):
     """
     update - информация от телеграм
     context - отдаём команды боту
     """
-    print("Вызван /start") # вывод в консоль
-    # print(update)
-    update.message.reply_text("Hello there")# вывод в телеграм
+    print("Вызван /start")  # вывод в консоль
+    
+    update.message.reply_text("Hello there")  # вывод в телеграм
 
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
